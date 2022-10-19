35d45ad8229838fd14fa8a2791c0b17923a18b43
yangzixuan <530691872@qq.com>
2022-10-19 22:39:24 +0800

    修改GameView

--- Assets/Scripts/View/GameView.cs
+++ Assets/Scripts/View/GameView.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,9 +6,14 @@ public class GameView : MonoBehaviour
 {
     public Button btnJump;
     public Button btnRewind;
-    public Image remainTime;
+    public Image remainBar;
     public Text pastTime;
-    
+
+    private float totalRewind = 300; //暂定可回溯总时间300s
+    private float remainRewind = 0;  //剩余回溯时间
+    private float minute = 0;
+    private float second = 0;
+    private float millisecond = 0;
     void Start()
     {
         btnJump.onClick.AddListener(OnJump);
@@ -25,8 +31,30 @@ public class GameView : MonoBehaviour
     {
         
     }
+    
     private void OnRewind()
     {
         
+    }
+    /// <summary>
+    /// 传入游戏开始以来的时间
+    /// </summary>
+    /// <param name="time"></param>
+    private void UpdateTime(float time)
+    {
+        minute = (int) (time / 60);
+        second = (int) (time - minute*60);
+        millisecond = (int) ((time - (int) time) * 1000);
+        pastTime.text = String.Format("{0:D2}:{1:D2}:{2:D2}",minute,second,millisecond);
+    }
+    /// <summary>
+    /// 设置剩余的可回溯时间
+    /// </summary>
+    /// <param name="restTime"></param>
+    private void SetRemainRewind(float restTime)
+    {
+        if (restTime < 0) return;
+        this.remainRewind = restTime;
+        remainBar.fillAmount = remainRewind / totalRewind;
     }
 }
