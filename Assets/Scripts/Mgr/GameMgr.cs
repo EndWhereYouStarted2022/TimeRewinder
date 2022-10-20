7e38c430cdf6f6f30d4267dc716809a30467b672
yangzixuan <530691872@qq.com>
2022-10-20 20:08:28 +0800

    修改游戏面板，调整参数等

--- Assets/Scripts/Mgr/GameMgr.cs
+++ Assets/Scripts/Mgr/GameMgr.cs
@@ -179,7 +179,7 @@ public class GameMgr : MonoSingleton<GameMgr>
 
     public void AddRewindPower(float power)
     {
-        _rewindPower += _rewindPower;
+        _rewindPower += power;
     }
 
     public void OnGUI()
